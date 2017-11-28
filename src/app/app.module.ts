b4f76f7b084fe463b8fdf5a9c9cc4119089e1092
Allin <allindier@gmail.com>
2017-11-27 21:07:03 -0600

    Control Skeleton
    Created the basic outline for the control component.  Set up the HTML to work with redux or whatnot in the future.  Have some slightly awkward HTML/JS to handle change of the input, but it seems okay for now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CanvasControlsComponent } from './canvas-controls/canvas-controls.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasControlsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
