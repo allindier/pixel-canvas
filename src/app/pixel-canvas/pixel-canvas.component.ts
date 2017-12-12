27dfb5d72a6346250b23fd04fe2294d032d6cd53
Allin <allindier@gmail.com>
2017-12-11 19:54:02 -0600

    Initial Keyboard Bindings
    Added some bindings to the canvas... not sure if this is really the right way to go with them.  Adding it in for now because it's nice to have.

--- src/app/pixel-canvas/pixel-canvas.component.ts
+++ src/app/pixel-canvas/pixel-canvas.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { NgRedux } from "@angular-redux/store";
+import { ChangeDetectionStrategy, Component, HostListener } from "@angular/core";
+import { ActionCreators } from "redux-undo";
+import { IAppState } from "../store";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -7,5 +10,24 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   templateUrl: "./pixel-canvas.html",
 })
 export class PixelCanvasComponent {
-  // Class currently does nothing
+
+  private static readonly Z = 90;
+
+  constructor(private $ngRedux: NgRedux<IAppState>) {}
+
+  @HostListener("document:keydown", ["$event"])
+  public keyInput(event: KeyboardEvent) {
+    // Don't want to intercept or intrude on any interactions with input elements
+    if ((event.target as HTMLElement).tagName === "INPUT") {
+      return;
+    }
+
+    if (event.ctrlKey && event.keyCode === PixelCanvasComponent.Z) {
+      if (event.shiftKey) {
+        this.$ngRedux.dispatch(ActionCreators.redo());
+      } else {
+        this.$ngRedux.dispatch(ActionCreators.undo());
+      }
+    }
+  }
 }
