66036843be29d889a636c1eee4fe869cc451d3e2
Allin <allindier@gmail.com>
2017-12-17 13:17:58 -0600

    Canvas Utility & Zoom Fix
    Created new class for holding logic related to the canvas specifically.  May be generalized to apply to pixel display later.  Also added in a fix to the click listener to accurately work when zoomed.

--- src/app/pixel-canvas/canvas/canvas.component.ts
+++ src/app/pixel-canvas/canvas/canvas.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from "rxjs/Subscription";
 import { IAppState } from "../../store";
 import { CanvasActions } from "../pixel-canvas.actions";
 import { IPixelCanvas } from "../pixel-canvas.store";
+import { CanvasUtility } from "../utility/canvas-utility";
 
 type Coordinate = [number, number];
 
@@ -15,29 +16,21 @@ type Coordinate = [number, number];
 })
 export class CanvasComponent implements AfterViewInit, OnDestroy {
 
-  private static readonly DASH_ARRAY = [8, 4];
-
+  @ViewChild("canvas") public canvasElem: ElementRef;
   public readonly canvasWidth = 400;
   public readonly canvasHeight = 400;
-  @ViewChild("canvas") public canvas: ElementRef;
 
-  private context: CanvasRenderingContext2D;
   private canvasData: IPixelCanvas;
   private subscription: Subscription;
   private pixelWidth: number;
   private pixelHeight: number;
   private zoom: number = 10;
+  private canvas: CanvasUtility;
 
   constructor(private readonly ngRedux: NgRedux<IAppState>, private actions: CanvasActions) { }
 
   public ngAfterViewInit() {
-    const canvasElement = this.canvas.nativeElement as HTMLCanvasElement;
-    const context = canvasElement.getContext("2d");
-    if (context === null) {
-      throw new Error("Unable to obtain 2D context for canvas.");
-    } else {
-      this.context = context;
-    }
+    this.canvas = new CanvasUtility(this.canvasElem.nativeElement, this.canvasWidth, this.canvasHeight);
 
     this.subscription = this.ngRedux.select<IPixelCanvas>(["canvas", "present"]).subscribe((canvas: IPixelCanvas) => {
       this.canvasData = canvas;
@@ -64,7 +57,13 @@ export class CanvasComponent implements AfterViewInit, OnDestroy {
       this.zoom--;
     }
 
-    this.redrawCanvas();
+    if (this.zoom < 10) {
+      this.zoom = 10;
+    } else if (this.zoom > 25) {
+      this.zoom = 25;
+    } else {
+      this.redrawCanvas();
+    }
   }
 
   /**
@@ -74,87 +73,22 @@ export class CanvasComponent implements AfterViewInit, OnDestroy {
    * @param event Mouse event from the HTML
    */
   public canvasClick(event: MouseEvent) {
-    const boundingRect = (this.canvas.nativeElement as HTMLCanvasElement).getBoundingClientRect();
+    const coordinate = this.canvas.getEventPosition(event);
 
-    const xValue = Math.floor((event.clientX - boundingRect.left)
-      * this.canvasData.width / this.canvasWidth);
-    const yValue = Math.floor((event.clientY - boundingRect.top)
-      * this.canvasData.height / this.canvasHeight);
+    const zoom = this.zoom / 10;
+    const xValue = Math.floor(coordinate[0] / zoom * this.canvasData.width / this.canvasWidth);
+    const yValue = Math.floor(coordinate[1] / zoom * this.canvasData.height / this.canvasHeight);
 
     this.ngRedux.dispatch(this.actions.canvasClick(xValue, yValue));
   }
 
-  /**
-   * Clears the contents of the canvas.  Does not account for transforms to the canvas.
-   */
-  private clearCanvas(zoom: number) {
-    this.context.clearRect(0, 0, this.canvasWidth / zoom, this.canvasHeight / zoom);
-  }
-
-  /**
-   * Draws the outlines of the pixels on the canvas.  Draws a dashed line, but does reset that back
-   * to its old value.
-   * @param zoom Zoom level to draw the outlines for.  Only affects the size of the line.
-   */
-  private drawPixelOutlines(zoom: number) {
-    const oldLineDash = this.context.getLineDash();
-    this.context.setLineDash(CanvasComponent.DASH_ARRAY);
-    this.context.lineWidth = 1 / zoom;
-
-    // TODO: Don't dupe the code
-    // Draw the horizontal lines
-    const barWidth = this.canvasWidth / this.canvasData.width;
-    let linePosition = barWidth;
-    for (let i = 1; i < this.canvasData.width; i++) {
-      this.drawLine(this.context, [linePosition, 0], [linePosition, this.canvasHeight]);
-
-      linePosition += barWidth;
-    }
-
-    // Draw the vertical lines
-    const barHeight = this.canvasHeight / this.canvasData.height;
-    linePosition = barHeight;
-    for (let i = 1; i < this.canvasData.height; i++) {
-      this.drawLine(this.context, [0, linePosition], [this.canvasWidth, linePosition]);
-
-      linePosition += barHeight;
-    }
-    this.context.setLineDash(oldLineDash);
-  }
-
-  /**
-   * Draws the pixels from the canvas data into the context
-   */
-  private drawPixels() {
-    this.canvasData.pixels.forEach((column, xIndex) => {
-      column.forEach((pixel, yIndex) => {
-        this.context.fillStyle = pixel;
-        this.context.fillRect(xIndex * this.pixelWidth, yIndex * this.pixelHeight,
-          this.pixelWidth, this.pixelHeight);
-      });
-    });
-  }
-
-  /**
-   * Draws a single line from the given start coordinate to the end.
-   * @param context Canvas context to draw the line on
-   * @param start Start coordinate to use
-   * @param end End coordinate to use
-   */
-  private drawLine(context: CanvasRenderingContext2D, start: Coordinate, end: Coordinate) {
-    context.beginPath();
-    context.moveTo(start[0], start[1]);
-    context.lineTo(end[0], end[1]);
-    context.stroke();
-  }
-
   private redrawCanvas() {
     const zoomValue = this.zoom / 10;
-    this.context.setTransform(zoomValue, 0, 0, zoomValue, 0, 0);
+    this.canvas.setZoom(zoomValue);
 
-    this.clearCanvas(zoomValue);
-    this.drawPixels();
-    this.drawPixelOutlines(zoomValue);
+    this.canvas.clearCanvas(zoomValue);
+    this.canvas.drawPixels(this.canvasData.pixels, this.canvasData.width, this.canvasData.height);
+    this.canvas.drawPixelOutlines(zoomValue, this.canvasData.width, this.canvasData.height);
   }
 
 }
