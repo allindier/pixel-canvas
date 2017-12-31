af8eeb61fae5ccf8d6b531ca7b009f0abe4e3d21
Allin <allindier@gmail.com>
2017-12-31 12:04:16 -0600

    Remove WA
    Pulled out WA code so that the module can be exported normally.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,9 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import { WasmUtil } from "./utils.wasm";
 
-const initAngular = () => {
-  if (environment.production) {
-    enableProdMode();
-  }
-  // tslint:disable-next-line no-console
-  platformBrowserDynamic().bootstrapModule(AppModule).catch((err) => console.log(err));
-};
-
-WasmUtil.initialize().then(initAngular).catch(initAngular);
+if (environment.production) {
+  enableProdMode();
+}
+// tslint:disable-next-line no-console
+platformBrowserDynamic().bootstrapModule(AppModule).catch((err) => console.log(err));
